4cda1e1dc86a078b957c5dccf5fc209bf5c46a99
Natalie Leong <natalieleong19@gmail.com>
2021-05-15 21:31:06 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactAnime from 'react-animejs';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [control, setControl ] = useState(null);
+
+  const [meta, setMeta] = useState({
+    control: control,
+    progress: 0,
+    currentTime: 0,
+    duration: 0
+  });
+ 
+  const { Anime, stagger } = ReactAnime;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Anime
+        initial={[
+          {
+            targets: '#Box',
+            translateX: 100,
+            easing: 'spring',
+            keyframes: [
+              {
+                translateX: 50
+              },
+              {
+                translateY: 50
+              },
+              {
+                translateX: 0
+              },
+              {
+                translateY: 0
+              }
+            ],
+            duration: 3500,
+            loop: true
+          }
+        ]}
+      >
+          <div id="Box" style={{height: 50, width: 50, background: '#d3d'}}>
+            HELLO
+            </div>
+      </Anime>
+      <Anime
+        control={control}
+        setMeta={setMeta}
+        animeConfig={{
+          autoplay: false,
+          duration: 1500,
+          easing: 'easeInOutSine'
+        }}
+        initial={[
+          {
+            targets: '.tl_square',
+            translateX: 250
+          },
+          {
+            targets: '.tl_circle',
+            translateX:250
+          },
+          {
+            targets: '.tl_triangle',
+            translateX: 250
+          }
+        ]}
+      >
+        <div
+          className="tl_square"
+          style={{height: 50, width: 50, background: '#d3f454'}}
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            className='tl_circle'
+            style={{
+              height: 50,
+              width: 50,
+              background: '#d3f454',
+              clipPath:"polygon(50% 0, 0 100%, 100%, 100%)"
+            }}
+          />
+          <div 
+            className="tl_triangle"
+            style={{
+              height: 50,
+              width: 50,
+              background: '#d3f454',
+              clipPath: 'polygon(50% 0, 0 100%, 100%, 100%)'
+            }}
+          />
+          <div
+            className="button"
+            onClick={()=>{
+              setControl("play")
+            }}
+            >
+              Play
+            </div>
+            <div className="button"
+            onClick={()=>{
+              setControl('pause')
+            }}>
+              Pause
+            </div>
+            <div
+              className="button"
+              onClick={()=>{
+                setControl('restart');
+              }}
+            >
+              Restart
+            </div>
+            <input type="range"
+              min='1'
+              max='100'
+              value={meta.progress || 0}
+              className='slider'
+              id="myRange"
+              onChange={e => console.log(setControl(['seek', e.currentTarget.value]))}
+              />
+        </div>
+      </Anime>
     </div>
+   
   );
 }
 
