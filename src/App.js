4745b0fa6b73157b635d5655fda12d9283f5f077
Natalie Leong <natalieleong19@gmail.com>
2021-05-16 15:58:45 +0800

    tested querySelector and Array.from().map() to split string for animation to work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactAnime from 'react-animejs';
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
   const [control, setControl ] = useState(null);
+  var string1 = 'Hello there!';
+  var string2 = 'My name is Natalie';
+ 
+  // const [ loadStatus, setLoadstatus ] = useState(true);
+  // var textWrapper1 = '';
+  // var textWrapper2 ='';
+
+  // useEffect(()=>{
+  //   textWrapper1 = document.querySelector('.h3');
+  //   console.log("textWrapper1",textWrapper1);
+  //   if(textWrapper1.textContent){
+  //     textWrapper1.innerHTML = textWrapper1.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='h3'>$&</span>")
+  //   }
+    
+  //   textWrapper2 = document.querySelector('.para');
+  //   console.log("textWrapper2",textWrapper2);
+  //   if(textWrapper2.textContent){
+  //     textWrapper2.innerHTML = textWrapper2.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='para'>$&</span>")
+  //   }
+  //   setLoadstatus(false);
+  // },[])
 
   const [meta, setMeta] = useState({
     control: control,
@@ -13,11 +34,70 @@ function App() {
     currentTime: 0,
     duration: 0
   });
- 
+
   const { Anime, stagger } = ReactAnime;
+  // if(loadStatus==false) {
+  //   var textWrapper1 = document.querySelector('.h3')?document.querySelector('.h3'):<span  class='h3' >Hello There!</span>;
+  //   console.log("textWrapper1",textWrapper1);
+  //   if(textWrapper1.textContent){
+  //     textWrapper1.innerHTML = textWrapper1?.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='h3'>$&</span>")
+  //   }
+    
+  //   var textWrapper2 = document.querySelector('.para')?document.querySelector('.para'):<span  class='para'>My name is Natalie</span>;
+  //   console.log("textWrapper2",textWrapper2);
+  //   if(textWrapper2.textContent){
+  //     textWrapper2.innerHTML = textWrapper2?.textContent?.replace(/([^\x00-\x80]|\w)/g, "<span class='para'>$&</span>")
+  //   }
+  // }
+  
+
+  
+  
   return (
     <div>
-      <Anime
+    <Anime
+        initial={[
+          {
+            targets: '.h3',
+            opacity: [0,1],
+            easing: 'easeOutExpo',
+            duration: 600,
+            offset: '-=775',
+            delay: (el, i) => 34 * (i+1)
+          },
+          {
+            targets: '.h3',
+            opacity: 0,
+            duration: 1000,
+            easing: 'easeOutExpo',
+            delay: 1000
+          },
+          {
+            targets: '.para',
+            opacity: [0,1],
+            easing: 'easeOutExpo',
+            duration: 600,
+            offset: '-=775',
+            delay: (el, i) => 34 * (i+1)
+          },
+          {
+            targets: '.para',
+            opacity: 0,
+            duration: 1000,
+            easing: 'easeOutExpo',
+            delay: 1000
+          },
+        ]}>
+        <span>{Array.from(string1).map((element) => {
+          return <span class='h3'>{element}</span>
+        })}</span>
+         <span>{Array.from(string2).map((element) => {
+          return <span class='para'>{element}</span>
+        })}</span>
+        
+      </Anime>
+      
+      {/* <Anime
         initial={[
           {
             targets: '#Box',
@@ -45,8 +125,8 @@ function App() {
           <div id="Box" style={{height: 50, width: 50, background: '#d3d'}}>
             HELLO
             </div>
-      </Anime>
-      <Anime
+      </Anime> */}
+      {/* <Anime
         control={control}
         setMeta={setMeta}
         animeConfig={{
@@ -122,7 +202,7 @@ function App() {
               onChange={e => console.log(setControl(['seek', e.currentTarget.value]))}
               />
         </div>
-      </Anime>
+      </Anime> */}
     </div>
    
   );
